e6ceb8b5309748e6996ca7f8daca07bdad1283ae
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-09-30 16:12:25 +0800

    Initial Commit

---
+++ C primer Plus/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C primer Plus
+//
+//  Created by 大娘 on 2020/9/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
