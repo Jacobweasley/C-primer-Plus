7832fbd5a053c7341c6cd0a9c453b8afb9270374
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-09-30 17:18:53 +0800

    数据与C 首次使用scant 获取 浮点型

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -9,12 +9,20 @@
 
 int main(void)
 {
-    int n, n2, n3;
+    float weight;
+    float value;
     
-    n = 5;
-    n2 = n * n;
-    n3 = n2 * n;
-    printf("n = %d, n squared = %d, n cbed = %d\n", n, n2, n3);
+    printf("Are you worth your weight in platinum?\n");
+    printf("Let's check it out.\n");
+    printf("Please enter your weight in pounds: ");
+    
+    scanf("%f", &weight);
+    
+    value = 1700.0 * weight * 14.5833;
+    printf("Your weight in platinum is worth $%.2f.\n", value);
+    printf("You are easily worth that ! If platinum prices drop, \n");
+    printf("eat more to maintain your value.\n");
     
     return 0;
+    
 }
