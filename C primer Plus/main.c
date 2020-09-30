f62ef3bef8b2fcc4cfb1780cc7cfe375d34eff78
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-09-30 16:53:52 +0800

    语义错误

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -13,10 +13,8 @@ int main(void)
     
     n = 5;
     n2 = n * n;
-    n3 = n2 * n2;
-    printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3);
+    n3 = n2 * n;
+    printf("n = %d, n squared = %d, n cbed = %d\n", n, n2, n3);
     
     return 0;
-    
-    
 }
