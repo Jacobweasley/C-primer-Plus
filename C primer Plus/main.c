408742321a34522b8c836e703c23d64dd32d7d4b
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 16:24:41 +0800

    定义了一个时间换算的方法.在这其中, 使用一个while 循环来做判断,只要输入 数字小于0 就会推出 while 循环.结束方法

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -4,23 +4,25 @@
 //
 //  Created by 大娘 on 2020/9/30.
 //
-
-
 #include <stdio.h>
-
+#define SEC_PER_MIN 60
 int main(void)
 {
-    int n = 0;
-    size_t intsize;
-    //这里的 size_t 返回的是一种无符号的返回值类型的整形
-    //这里使用到的是 typedef 的机制,即将 C语言中原本就带有的类型起别名.
-    //在当前的使用环境中,该 size_t 的就就相当于 unsigned int .
+    int sec, min, left;
     
-    intsize = sizeof  (int);
+    printf("Convert seconds to minutes an seconds!\n");
+    printf("Enter the number of seconds (<=0 to quit): \n");
+    scanf("%d", &sec);
+    while (sec > 0)
+    {
+        min = sec / SEC_PER_MIN;
+        left = sec % SEC_PER_MIN;
+        printf("%d seconds is %d minutes, %d seconds.\n", sec, min , left );
+        printf("Enter naxt value (<=0 to quit): \n");
+        scanf("%d", &sec);
+    }
+    printf("Done!\n");
     
-    printf("n = %d, n has %zd bytes; all ints have %zd bytes.\n", n , sizeof n , intsize);
-    //在C语言中的printf() 函数中使用 %zd 来表示转换过的int 整形,用来显示 size_t 类型的输入输出
     return 0;
-    
     
 }
