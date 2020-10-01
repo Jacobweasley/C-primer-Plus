8f195b1064c25a42c3d6ec39c5189bf8d8725602
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 16:09:20 +0800

    在C语言中 可以使用 typedef 的机制来自己定义类型,该机制的具体原理在后续将得到补充 并且在 C语言中, 使用 sizeof() 函数来检测该入参中的值所占的实际内存大小, 注意使用 sizeof() 函数来检测 char[] 字符串的大小时,该字符串中的 “\0” 的空字符 也会算入该 sizeof 的大小计量中

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -5,14 +5,19 @@
 //  Created by 大娘 on 2020/9/30.
 //
 
+
 #include <stdio.h>
 
 int main(void)
 {
-    int top, score;
+    int n = 0;
+    size_t intsize;
+    //这里的 size_t 返回的是一种无符号的返回值类型的整形
+    //这里使用到的是 typedef 的机制,即将 C语言中原本就带有的类型起别名.
+    //在当前的使用环境中,该 size_t 的就就相当于 unsigned int .
+    intsize = sizeof  (int);
     
-    top = score = - (2 + 5) * 6 + (4 + 3 * (2 + 3));
-    printf("top = %d,  score = %d\n", top , score);
+    printf("n = %d, n has %zd bytes; all ints have %zd bytes.\n", n , sizeof n , intsize);
     
     return 0;
     
