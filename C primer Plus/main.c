4d1aed6979d5021937f7c7dd62a57d080551aaa3
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 15:54:00 +0800

    在 printf () 函数的中使用 %f 表示 输入输出浮点型 ,同样也可以使用 在 % 与 f 之间 添加带小数点的数字来表示 精确的w位数

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -6,30 +6,14 @@
 //
 
 #include <stdio.h>
-#define SQUARES 64
 
 int main(void)
 {
-    const double CROP = 2E16;
-    double current, total;
-    int count = 1;
-    
-    printf("square     grains      total      ");
-    printf("fraction of  \n");
-    printf("           added       grains     ");
-    printf("world total\n");
-    total = current = 1.0;
-    printf("%4d  %13e %12.2e  %12.2e\n", count, current, total, total / CROP);
-    while (count < SQUARES)
-    {
-        count = count + 1;
-        current = 2.0 * current;
-        total = total + current;
-        printf("%4d  %13.2e  %12.2e  %12.2e\n", count, current, total, total / CROP);
-        
-    }
-    
-    printf("That's all.\n");
+    printf("integer division: 5/4   is  %d  \n", 5 / 4);
+    printf("integer division: 6/3   is  %d  \n", 6 / 3);
+    printf("integer division: 7/4   is  %d  \n", 7 / 4);
+    printf("integer division: 7./4   is  %1.2f  \n", 7. / 4.);
+    printf("mixed division:   7./4   is  %1.2f  \n", 7. / 4);
     
     return 0;
     
