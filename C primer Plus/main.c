a47bc9ad62f0e2198637025a6ea0fe44b3b5156a
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 15:22:11 +0800

    使用 while 循环 运行 *  运算符

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -6,14 +6,15 @@
 //
 
 #include <stdio.h>
+
 int main(void)
 {
-    int jane, tarzan, cheeta;
-    
-    cheeta = tarzan = jane = 68;
-    printf("                  cheeta   tarzan      jane\n");
-    printf("First round score %4d   %8d  %8d\n", cheeta, tarzan, jane);
+    int num = 1;
+    while (num < 21){
+        printf("%4d %6d\n", num, num * num);
+        num = num + 1;
+    }
     
     return 0;
+    
 }
-
