43e564fb815a3fd8198c4af7fbdb13146346ce7b
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 15:58:24 +0800

    在运行时的先后顺序问题

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-    printf("integer division: 5/4   is  %d  \n", 5 / 4);
-    printf("integer division: 6/3   is  %d  \n", 6 / 3);
-    printf("integer division: 7/4   is  %d  \n", 7 / 4);
-    printf("integer division: 7./4   is  %1.2f  \n", 7. / 4.);
-    printf("mixed division:   7./4   is  %1.2f  \n", 7. / 4);
+    int top, score;
+    
+    top = score = - (2 + 5) * 6 + (4 + 3 * (2 + 3));
+    printf("top = %d,  score = %d\n", top , score);
     
     return 0;
+    
     
 }
