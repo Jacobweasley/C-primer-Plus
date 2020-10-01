52ba1fa5cec51b8a584794c0ebd5febfd186819b
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 15:18:31 +0800

    复制运算符 自右向左运行

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -6,26 +6,14 @@
 //
 
 #include <stdio.h>
-#define ADJUST 7.3
 int main(void)
 {
-    const double SCALE = 0.333;
-    double shoe, foot;
+    int jane, tarzan, cheeta;
     
-    printf("Shoe size (men's)   foot length\n");
-    shoe = 3.0;
-    while (shoe < 18.5) {
-        foot = SCALE * shoe + ADJUST;
-        printf("%10.1f %15.2f inches\n", shoe, foot);
-        shoe = shoe + 1.0;
-    }
-    //这里使用了 while 循环,即在变量 shoe 没有大于 18.5的情况下持续运行该循环
-    //在该循环中,先是算出该鞋码对应的大小的脚的尺寸.
-    //每次村还都是将对应的鞋码与脚的大小,打印出来.
-    //在每次循环中,shoe 变量自加 1 .
-    printf("If the shoe fits, wear it. \n");
+    cheeta = tarzan = jane = 68;
+    printf("                  cheeta   tarzan      jane\n");
+    printf("First round score %4d   %8d  %8d\n", cheeta, tarzan, jane);
     
     return 0;
 }
-
 
