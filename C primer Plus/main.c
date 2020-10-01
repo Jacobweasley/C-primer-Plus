d4a4a5508de4ad405a861727047ae4729d42c65d
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-01 16:41:03 +0800

    在C语言中 ++ -- 运算符中,如果是单独使用的话,他们被放在数的前面又或是放到后米,都没有什么且别,涉及到双目运算符的时候,他们的区别就会出现. 在 ++ -- 运算符放到数字的前面是,表示在操作前 + 1, 而如果 ++ -- 放到 数的后面的时候表示 先进行操作后在 + 1.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -7,17 +7,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int ultra = 0, super = 0;
+    int a = 1, b = 1;
+    int a_post, pre_b;
     
-    while (super < 5)
-    {
-        super++;
-        ++ultra;
-        printf("super = %d, ultra = %d \n", super , ultra);
-        
-    }
+    a_post = a++;
+    pre_b = ++b;
+    
+    printf("a a_post   b   pre_b  \n");
+    printf("%1d %5d %5d %5d\n", a , a_post, b , pre_b);
     
     return 0;
-    
-    
 }
