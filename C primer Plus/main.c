0f2e2003e1fed7feee3e603609589471afaca8be
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-03 14:02:37 +0800

    类型转换即,在将一个 char 类型的字符, 转换成一个整形类型时,其实转变成的事该 字符对应的 ASCLL 码表对应的 10 进制字符.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -7,14 +7,22 @@
 #include <stdio.h>
 int main(void)
 {
-    int a = 1, b = 1;
-    int a_post, pre_b;
+    char ch;
+    int i;
+    float fl;
     
-    a_post = a++;
-    pre_b = ++b;
+    fl = i = ch = 'C';
+    printf("ch = %c, i = %d, fl = %2.2f\n", ch, i, fl);
+    ch = ch + 1;
+    i = fl + 2 * ch;
+    fl = 2.0 * ch + i;
+    printf("ch = %c, i = %d, fl = %2.2f\n", ch, i, fl);
+    ch = 1107;
+    printf("Now ch = %c\n", ch);
+    ch = 80.89;
+    printf("Now ch = %c\n", ch);
     
-    printf("a a_post   b   pre_b  \n");
-    printf("%1d %5d %5d %5d\n", a , a_post, b , pre_b);
+    
     
     return 0;
 }
