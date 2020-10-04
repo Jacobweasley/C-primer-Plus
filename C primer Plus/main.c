9d7b77104d23aa3418fc3547b79b39895330f572
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-04 19:14:36 +0800

    getchar() 函数的作用就是即刻的检测你按下的第一个字符,并返回该字符,也就是说可以用一个变量来接收这样的getchar() 返回的键盘按键映射.然后储存,而putchar 函数的作用就是输出 入参中的字符. 注意的的是 getchar 和 putchar 都是只会输入输出一个字符的,要是想要连续的输入输出字符的话,就需要搭配使用 while 循环来使 getchar 与 putchar 能够连续的处理不用的字符,即输入一个字符处理处理一个字符.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -5,26 +5,21 @@
 //  Created by 大娘 on 2020/9/30.
 //
 #include <stdio.h>
+#define SPACE ' '
 int main(void)
 {
-    const int FREEZING = 0;
-    float temperatre;
-    int cold_days = 0;
-    int all_days = 0;
+    char ch;
     
-    printf("Enter the list of daily low temperatures.\n");
-    printf("Use Celsius, and enter q to quit.\n");
-    while (scanf("%f", &temperatre) == 1)
+    ch = getchar();
+    while(ch != '\n')
     {
-        all_days++;
-        if (temperatre < FREEZING)
-            cold_days++;
+        if (ch == SPACE)
+            putchar(ch);
+        else
+            putchar(ch + 1);
+        ch = getchar();
     }
-    if (all_days != 0)
-        printf("%d days total: %.1f%% were below freezing.\n", all_days, 100.0 * (float) cold_days / all_days);
-    if (all_days == 0)
-    {
-        printf("No data entered!\n");
-    }
+    putchar(ch);
     
+    return 0;
 }
