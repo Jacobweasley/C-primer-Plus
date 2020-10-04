2f957bdf189976886ff73174cf8183a48bb4bba7
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-04 17:18:09 +0800

    %.0f 这样的事表示精确到小数点后面的0位也就是没有小数点

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -5,18 +5,30 @@
 //  Created by 大娘 on 2020/9/30.
 //
 #include <stdio.h>
-#define ROWS 6
-#define CHARS 10
+#define SIZE 10
+#define PAR 72
 int main(void)
 {
-    int row;
-    char ch;
+    int index, score[SIZE];
+    int sum = 0;
+    float average;
     
-    for (row = 0; row < ROWS; row++) {
-        for (ch = 'A'; ch < ('A' + CHARS); ch++) {
-            printf("%c", ch);
-        }
-        printf("\n");
+    printf("Enter %d golf scores:\n", SIZE);
+    for (index = 0; index < SIZE ; index++ ) {
+        scanf("%d", &score[index]);
     }
+    printf("The scores read in are as follows:\n");
+    for (index = 0; index < SIZE; index++) {
+        printf("%5d", score[index]);
+    }
+    printf("\n");
+    for (index = 0; index < SIZE; index++) {
+        sum += score[index];
+    }
+    average = (float) sum / SIZE;
+    printf("Sum of scores = %d, average = %.2f\n", sum, average);
+    printf("That's a handicap of %.0f.\n", average - PAR);
+    
     return 0;
+    
 }
