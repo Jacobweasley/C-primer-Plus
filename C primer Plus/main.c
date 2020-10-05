5f338e63420a39356e1ab7bd4075842cbc22f0b0
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-05 16:55:09 +0800

    这这个程序其中的 inword 的作用是表示是否在单词中,因为在英文中 如果是输入的话只有在单词与不是在单词输入中这两种,所以这里的 联合条件判断是为了形成一个 行 toggle 一样的作用,

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -5,18 +5,41 @@
 //  Created by 大娘 on 2020/9/30.
 //
 #include <stdio.h>
-#define PERIOD '.'
- int main(void)
-{
-    char ch;
-    int charcount = 0;
+#include <ctype.h>
+#include <stdbool.h>
+#define STOP '|'
+
+int main(void){
+    char c;
+    char prev;
+    long n_chars = 0L;
+    int n_lines = 0;
+    int n_words = 0;
+    int p_lines = 0;
+    bool inword = false;
     
-    while ((ch = getchar()) != PERIOD) {
-        if (ch != '"' && ch != '\'')
-            charcount++;
+    printf("Enter text to be analyzed (| to terminate):\n");
+    prev = '\n';
+    while ((c = getchar()) != STOP){
+        n_chars++;
+        if (c == '\n')
+            n_lines++;
+        if (!isspace(c) && !inword) {
+            inword = true;
+            n_words++;
+        }
+        if (isspace(c) && inword){
+            inword = false;
+        }
+        prev = 'c';
+            
     }
-    printf("There are %d non-quote characters.\n", charcount);
+    
+    if (prev != '\n') {
+        p_lines = 1;
+    }
+    printf("characters = %ld, words = %d, lines = %d, ", n_chars, n_words, n_lines);
+    printf("partila lines = %d\n", p_lines);
     
     return 0;
-    
 }
