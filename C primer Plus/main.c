ca33bb7dbee6449b5672de6064b39f18aa69d160
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-06 13:55:50 +0800

    在 C语言 中的 switch 语句使穿透性的,也就是说要想要实现单个对单个的开关的话,就必须要使用 break 语句配合使用.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -5,21 +5,51 @@
 //  Created by 大娘 on 2020/9/30
 
 #include <stdio.h>
-int main(void){
-    float length, width;
+#include <ctype.h>
+
+int main(void)
+{
+    char ch;
     
-    printf("Enter the length of the rectangle:\n");
-    while (scanf("%f", &length) == 1) {
-        printf("Length = %0.2f:\n", length);
-        printf("Enter its width:\n");
-        if (scanf("%f", &width) != 1)
-            break;
-        printf("Width = %0.2f:\n", width);
-        printf("Area = %0.2f:\n", length * width);
-        printf("Enter the length of the rectangle:\n");
+    printf("Give me a letter of the alphabet, and I will give ");
+    printf("an animal name \nbeginning with that letter.\n");
+    printf("Please type in a letter; type # to end my act.\n");
+    while ((ch = getchar()) != '#')
+    {
+        if ('\n' == ch)
+            continue;
+        if (islower(ch))
+            switch (ch) {
+                case 'a':
+                    printf("argali, a wild sheep of Asia\n");
+                    break;
+                case 'b':
+                    printf("babirusa, a wild pig of Malay\n");
+                    break;
+                case 'c':
+                    printf("coati, racoonlike mammal\n");
+                    break;
+                case 'd':
+                    printf("desman, aquatic, molelike critter\n");
+                    break;
+                case 'e':
+                    printf("exhidna, the spiny anteater\n");
+                    break;
+                case 'f':
+                    printf("fisher, brownish marten\n");
+                    break;
+                default:
+                    printf("That's a stumper!\n");
+                    break;
+            }
+        else
+            printf("I recognize only lowercase letters.\n");
+        while (getchar() != '\n') {
+            continue;
+        }
+        printf("Please type another letter or a #.\n");
     }
-    
-    printf("Done.\n");
+    printf("Bye!\n");
     
     return 0;
 }
