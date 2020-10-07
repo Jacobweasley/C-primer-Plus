a7d9bdc5be82a0590a3ece8af88837ea96eae679
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-07 13:45:12 +0800

    在循环检测 getchar() 函数的中 适应判断语句 if  来检查输入的 行数与个数 是否正确输入.如果输入不正确就直接退出. 而后 在输入完成后并没有使用 \n 号的话,就持续监测,直到输入 \n 为止,但是该 getchar() 中所获取的 字符 所存储的缓冲区并不会被实际的实际使用,在最终 捕获到 \n 后其余的字符就会被抛弃

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -5,18 +5,22 @@
 //  Created by 大娘 on 2020/9/30
 
 #include <stdio.h>
-void display (char cr, int lines, int width);
+void display(char cr, int lines, int width);
 int main(void)
 {
     int ch;
     int rows, cols;
-    printf("Enter a character and two integers:\n");
+    
+    printf("Enter a character and two integers: \n");
     while ((ch = getchar()) != '\n') {
-        scanf("%d %d", &rows, &cols);
+        if (scanf("%d %d", &rows, &cols) != 2)
+            break;
         display(ch, rows, cols);
+        while (getchar() != '\n') {
+            continue;
+        }
         printf("Enter another character and two integers;\n");
         printf("Enter a newline to quit.\n");
-        
     }
     printf("Bye.\n");
     
@@ -27,10 +31,10 @@ void display(char cr, int lines, int width)
 {
     int row, col;
     
-    for (row = 1; row <= lines; row++)
-    {
-        for (col = 1; col <= width; col++)
-        putchar(cr);
+    for (row = 1; row <= lines; row++) {
+        for (col = 1; col <= width; col++) {
+            putchar(cr);
+        }
         putchar('\n');
     }
 }
