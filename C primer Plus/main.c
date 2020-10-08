7292e6468ff5ab32047d6e8dadbc376cb53d6b6c
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-08 15:46:55 +0800

    在 main 外声明多个函数,并创建不同的开关来对应不同的函数,同样的事使用 getchar() 函数来检测输入的字符. 但是这个 getchar() 是包裹在另外一个函数里面的,所以在该函数的return ch 中检测是否为 q .如果是则结束主函数,因为主函数当中只有循环直线的 while 函数,该函数在开启字符检测的情况下,抛出string 来表明选项. 在根据用户的选项,来作出反应,来执行不用的函数.于实现不用的方法.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -3,64 +3,80 @@
 //  C primer Plus
 //
 //  Created by 大娘 on 2020/9/30
-
 #include <stdio.h>
-#include <stdbool.h>
+char get_choice(void);
+char get_first(void);
+int get_int(void);
+void count(void);
 
-long get_long(void);
-
-bool bad_limits(long begin, long end, long low, long hight);
-double sum_squares(long a, long b);
-int main(void)
-{
-    const long MIN = -10000000L;
-    const long MAX = +10000000L;
-    long start;
-    long stop;
-    double answer;
+int main(void){
+    int choice;
     
-    printf("This program computes the sum of the squares of "
-           "integers in a range.\nThe lower bound should not "
-           "be less than -10000000 and\nthe upper bound "
-           "should not be more than +10000000.\nEnter the "
-           "limits (enter 0 for both limits to quit):\n"
-           "lower limit: ");
-    start = get_long();
-    printf("upper limit: ");
-    stop = get_long();
-    while (start != 0 || stop != 0)
-    {
-        if (bad_limits(start, stop, MIN, MAX))
-            printf("Please try again.\n");
-        else
-        {
-            answer = sum_squares(start, stop);
-            printf("The sum of the squares of the integers ");
-            printf("from %ld to %ld is %g\n",
-                   start, stop, answer);
+    while ((choice = get_choice()) != 'q'){
+        switch (choice){
+            case 'a': printf("Buy low, sell high.\n");
+                break;
+            case 'b': putchar('\a');
+                break;
+            case 'c': count();
+                break;
+            default: printf("Program error!\n");
+                break;
         }
-        printf("Enter the limits (enter 0 for both "
-               "limits to quit):\n");
-        printf("lower limit: ");
-        start = get_long();
-        printf("upper limit: ");
-        stop = get_long();
     }
-    printf("Done.\n");
+    printf("Bye.\n");
     
     return 0;
 }
 
-long get_long(void)
-{
-    long input;
+void count(void){
+    int n, i;
+    
+    printf("Count how far? Enter an integer:\n");
+    n = get_int();
+    for (i = 1; i <= n; i++){
+        printf("%d\n", i);
+    }
+    while (getchar() != '\n') {
+        continue;
+    }
+}
+
+
+char get_choice(void){
+    int ch;
+    
+    printf("Enter the letter of your choice:\n");
+    printf("a. advice         b. bell\n");
+    printf("c. count          q. quit\n");
+    ch = get_first();
+    while ((ch < 'a' || ch > 'c') && ch != 'q') {
+        printf("Please respond with a, b, c, or q.\n");
+        ch = get_first();
+    }
+    
+    return ch;
+}
+
+
+char get_first(void){
+    int ch;
+    
+    ch = getchar();
+    while (getchar() != '\n'){
+        continue;
+    }
+    
+    return ch;
+}
+
+int get_int(void){
+    int input;
     char ch;
     
-    while (scanf("%ld", &input) != 1)
-    {
-        while ((ch = getchar()) != '\n') {
+    while (scanf("%d", &input) != 1){
+        while ((ch = getchar()) != '\n')
             putchar(ch);
-        }
         printf(" is not an integer.\nPlease enter an ");
         printf("integer value, such as 25, -178, or 3: ");
     }
@@ -68,33 +84,4 @@ long get_long(void)
     return input;
 }
 
-double sum_squares(long a, long b){
-    double total = 0;
-    long i;
-    
-    for (i = a; i <= b; i++)
-    	total += (double) i * (double) i;
-    
-    return total;
-}
 
-bool bad_limits(long begin, long end, long low, long high)
-{
-    bool not_good = false;
-    
-    if (begin > end )
-    {
-        printf("%ld isn't smaller than %ld.\n", begin, end);
-        not_good = true;
-    }
-    if (begin < low || end < low ) {
-        printf("Values must be %ld or greater.\n", low);
-        not_good = true;
-    }
-    if (begin > high || end > high) {
-        printf("Values must be %ld or less.\n", high);
-        not_good = true;
-    }
-    
-    return not_good;
-}
