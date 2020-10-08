f3d184261a5951e70caf5655d55bf5a89fe2163f
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-08 20:24:08 +0800

    这里体现的是一种剧中的算法, 这种算法,的作用原理是,因为书写字符串的的地方是自己定义的,所以是实现知道该公示牌的每行的长度的,所以我们又知道每句话的长度,所以在实际上我们只需要将原先行数的减去字符串的长度然后再除 2 所以剩下的字符就可以正好的将 每行对应的字符串就推到中间的位置.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -4,29 +4,40 @@
 //
 //  Created by 大娘 on 2020/9/30
 #include <stdio.h>
-#define NAME "GEGATHINK, INC."
+#include <string.h>
+#define  NAME "GIGATHINK, INC."
 #define ADDRESS "101 Megabuck Plaza"
-#define PLACE "Megapolis, CA 94904"
-#define WIDTH 40
+#define PLACE "Megapolis, CA  94904"
+#define WICTH 40
+#define SPACE ' '
 
-void starbar(void);
+void show_n_char (char ch, int num);
 
-int main(void)
-{
-    starbar();
+int main(void){
+    int spaces;
+    
+    show_n_char('*', WICTH);
+    putchar('\n');
+    show_n_char(SPACE, 12);
     printf("%s\n", NAME);
+    spaces = (WICTH - strlen(ADDRESS)) / 2;
+    
+    show_n_char(SPACE, spaces);
     printf("%s\n", ADDRESS);
+    show_n_char(SPACE, (WICTH - strlen(PLACE)) / 2);
+    
     printf("%s\n", PLACE);
-    starbar();
+    show_n_char('*', WICTH);
+    putchar('\n');
     
     return 0;
+    
 }
 
-void starbar(void)
+void show_n_char(char ch, int num)
 {
     int count;
     
-    for (count = 1; count <= WIDTH; count++)
-    putchar('*');
-    putchar('\n');
+    for (count = 1; count <= num; count++)
+    putchar(ch);
 }
