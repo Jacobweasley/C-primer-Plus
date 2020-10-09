d6b6f567c1b9cb54f27875be3892c57bdef5aefe
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-09 15:38:16 +0800

    这是一个将十进制转换成二进制的算法,需要首先该清楚十进制转二进制的运算方法想,其实在现实中十进制转二进制的运算可以算是一种是非的结果,很简单的,实际上只是不停的求它的 / 2 的值就好了,如果求出他的值是带小数的,就写 1 如果求出的值是整数的话就写0. 注意这个持续算下起的的意思是对结果进行 /  2 的运算而不是自己本身.也就是说,要停的对 入参进行 / 2 所以 然后就看这个结果是否是带小数位的,如果就写1 如果没有就写0.直到算到1为止,算到后面总会是只剩下 1 的,所以酒直接写1结束运算. 除了这个二进制的算法意外,这个例子中我们还需要注意的是,就是putcher 函数,在这个并没有新建一个数组来存储这个从后到前的运算结果,而是直接用putchar 函数输出,但是在在这里并没有出现在程序运行时持续的推出字符的情况时因为,这个二进制函数中的putchar () 是建立在有缓冲区这个概念的基础上的,putchar 的输出其实并不是一执行putchar 就会在 屏幕上打印出 一个字符的,而是先在缓冲区总输出一个字符,而这字符的输出有点像是键盘输入的缓冲区的运算原理正好相反,因为在我们输入字符的时候,先输入的字符总是在前,而后输入的总是排在后面,这是因为缓冲区对 getchar 与 putchar 的运行原理是相同的,都是从后往前输入到屏幕上的,想象一下,你 getchar 的字符先 是存放在缓冲区中,然后后面输入的字符总是插入到已经存在与缓冲区中的字符的前面,所以等到需要将缓冲区的字符真正的输入到屏幕上的时候,就需要从缓冲区中的最后一项开始往前输出. 在这个递归总也是同理,位于递归最后放的 putchar 最先在缓冲区中放入字符 后return 的putchar 就放到原来缓冲区中已有的字符的前面,等到最后的函数return以后,在 主函数中的 putchar 输入  \n 的情况下,才开始真正的开始输入到屏幕上.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -3,54 +3,40 @@
 //  C primer Plus
 //
 //  Created by 大娘 on 2020/9/30
+
+
+
 #include <stdio.h>
-long fact(int n);
-long rfact(int  n);
+
+void to_bingary (unsigned long n);
+
 int main(void)
 {
-    int num;
-    
-    printf("This program calculates factorials.\n");
-    printf("Enter a value in the range 0-12 (q to quit):\n");
-    while (scanf("%d", &num) == 1)
-    {
-        if (num < 0)
-            printf("Nonegative numbers, please.\n");
-        else if (num > 12)
-            printf("Keep input under 13.\n");
-        else {
-            printf("loop: %d factorial = %ld\n",
-                   num, fact(num));
-            printf("recursion: %d factorial = %ld\n",
-                   num, rfact(num));
-        }
-        printf("Enter a value in the range 0-12 (q to quit):\n");
+    unsigned long number;
+    printf("Enter an integer (q to quit):\n");
+    while (scanf("%lu", &number) == 1) {
+        printf("Binary equivalent: ");
+        to_bingary(number);
+        putchar('\n');
+        printf("Enter an integer (q to quit): \n");
+        
     }
-    printf("Bye.\n");
+    printf("Done.\n");
     
     return 0;
 }
 
-long fact(int n)
+void to_bingary(unsigned long n)
 {
-    long ans;
+    int r;
     
-    for (ans = 1; n > 1; n--)
-    ans *= n;
+    r = n % 2;
+    if (n >= 2)
+        to_bingary(n / 2);
+    putchar(r == 0 ? '0' : '1');
     
-    return 0;
+    return;
 }
 
 
-long rfact(int n)
-{
-    long ans;
-    
-    if (n > 0)
-        ans = n * rfact(n - 1);
-    else
-        ans = 1;
-    
-    return ans;
-}
 
