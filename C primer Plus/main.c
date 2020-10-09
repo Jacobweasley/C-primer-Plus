13b1c3f71a27d867d72f19c5e7dc88e510d6321c
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-09 13:13:33 +0800

    使用 函数的时候是需要将函数的原型定义的时候的所有的 入参都填入,因为不然编译器会编译不通过.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -4,32 +4,17 @@
 //
 //  Created by 大娘 on 2020/9/30
 #include <stdio.h>
-int imin(int, int);
+int imax(int , int);
 
 int main(void)
 {
-    int evil1, evil2;
-    
-    printf("Enter a pair of integers (q to quit):\n");
-    while (scanf("%d %d", &evil1, &evil2))
-    {
-        printf("The lesser of %d and %d is %d.\n",
-               evil1, evil2, imin(evil1, evil2));
-        printf("Enter a pair of integers (q to quit) :\n");
-    }
-    printf("Bye.\n");
-    
+    printf("The maximum of %d and %d is %d.\n", 3, 5, imax(3, 5));
+    printf("The maximun of %d and %d is %d.\n", 3, 5, imax(3.0, 5.0));
     return 0;
 }
 
-int imin(int n, int m)
+int imax(int n, int m)
 {
-    int min;
-    
-    if (n<m) {
-        min = n;
-    } else {
-        min = m;
-    }
-    return min;
+    return (n > m ? n : m);
 }
+
