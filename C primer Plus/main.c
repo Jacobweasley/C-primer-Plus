9d26838477a57eea6892e33e50195ee72dfc753b
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-10 15:31:52 +0800

    数组的名称就是数组中第一个项的内存地址,也就是说数组名就是一个指向数组第一个项的指针.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-#define SIZE 4
+#define MONTHS 12
+
 int main(void)
 {
-    short dates[SIZE];
-    short * pit;
-    short index;
-    double bills[SIZE];
-    double * ptf;
-    pit = dates;
-    ptf = bills;
-    printf("%23s %15s\n", "short", "double");
-    //这个打印语句实际上只是单纯的打印字符串的语句而已
-    for (index = 0; index < SIZE; index++)
-    printf("pointers + %d: %10p %10p\n", index, pit + index, ptf + index);
+    int days[MONTHS] = { 31, 28, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
+    int index;
     
+    for (index = 0; index < MONTHS; index++)
+    printf("Month %2d has %d days.\n", index + 1, *(days + index));
+    //这里并没有使用下标的方式来遍历数组,但是就在这样的写法等同于 下标 但是这样的写法并不如 下标明了
+    //这样写的原理是 数组的名称就是数组中第一个项的内存地址,所以也就相当于指针 + 1的原理一样,其前面的 * 操作符是用来取值的.
     return 0;
 }
-
