497f0c13d5e5f719d19b9819c786860c7b15ed54
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-10 17:28:32 +0800

    在 C语言中 在函数中调用入参的数组类型是取值操作,因为 C语言认为为了入参而将一个数组类型拷贝一份是不划算的,所以在 C语言当中,所有的入参数组类型都是取址操作.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -4,13 +4,13 @@ void show_array(const double ar[], int n);
 void mult_array(double ar[], int n, double mult);
 int main(void)
 {
-    double dip[SIZE] = { 20.0, 17.66, 8.2, 15.3, 22.22 };
+    double dip[SIZE] = { 20.0, 17.66,8.2, 15.3, 22.22 };
     printf("The original dip array:\n");
     show_array(dip, SIZE);
     mult_array(dip, SIZE, 2.5);
-    printf("The dip array after calling mult_array():\n");
+    printf("The dip array agter calling mult_array():\n");
     show_array(dip, SIZE);
-
+    
     return 0;
 }
 
@@ -18,16 +18,17 @@ void show_array(const double ar[], int n)
 {
     int i;
     
-    for (i = 0; i < n ; i++)
-    printf("%8.3f ", ar[i]);
+    for (i = 0; i < n; i++)
+    printf("%8.3f", ar[i]);
     putchar('\n');
+    
 }
 
-
-void mult_array (double ar[], int n, double mult)
+void mult_array(double ar[], int n, double mult)
 {
     int i;
     
     for (i = 0; i < n; i++)
     ar[i] *= mult;
+    
 }
