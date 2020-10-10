439e7842d736a7c4a566e3e89ee2ca375c2a596a
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-10 12:56:45 +0800

    在 C语言中 使用for 循环来遍历 数组, 在 C语言中并没有像 Swift 一样的 字典以及 set 在 C语言中的array 的下标也是包裹在[ ] 当中的.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
-void interchange(int * u, int * v);
+#define MONTHS 12
 
 int main(void)
 {
-    int x = 5, y = 10;
-    printf("Originally x = %d and y = %d.\n", x, y);
-    interchange(&x, &y);
-    printf("Now x = %d and y = %d.\n", x, y );
+    int days[MONTHS] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int index;
+    
+    for (index = 0; index < MONTHS; index++)
+    {
+        printf("Month %2d has %2d days.\n", index + 1, days[index]);
+    }
     
     return 0;
-}
-
-
-void interchange(int * u, int * v){
-    int temp;
-    temp = *u;
-    *u = *v;
-    *v = temp;
 }
