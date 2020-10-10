252a75d626a0597e8c15dfff13d9dab85b07efa7
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-10 15:44:48 +0800

    sizeof 函数同来检测入参所占的内存大小的原理应该是 检测入参的指针所指向的一段连续的数据,同时检测这个连续内存空间的大小.最后才 return 一个标准行的 整形,在 stdio.h 的函数中 表示为 %zd 类型,其中的 z 表示 size - t 类型,属于一个标准型,一个整形的绝对值吧

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
-#define MONTHS 12
-
+#define SIZE 10
+int sum(int ar[], int n);
 int main(void)
 {
-    int days[MONTHS] = { 31, 28, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
-    int index;
+    int marbles[SIZE] = { 20, 10, 5, 39, 4, 16, 19, 26, 31, 20};
+    long answer;
     
-    for (index = 0; index < MONTHS; index++)
-    printf("Month %2d has %d days.\n", index + 1, *(days + index));
-    //这里并没有使用下标的方式来遍历数组,但是就在这样的写法等同于 下标 但是这样的写法并不如 下标明了
-    //这样写的原理是 数组的名称就是数组中第一个项的内存地址,所以也就相当于指针 + 1的原理一样,其前面的 * 操作符是用来取值的.
+    answer = sum(marbles, SIZE);
+    printf("The total number of marbles is %ld.\n", answer);
+    printf("The size of marbles is %zd bytes.\n", sizeof(marbles));
+    
     return 0;
+}
+
+int sum(int ar[], int n)
+{
+    int i;
+    int total = 0;
+    
+    for (i = 0; i < n; i++ )
+    total += ar[i];
+    printf("The size of ar is %zd bytes.\n", sizeof ar);
+    
+    return total;
+    
 }
