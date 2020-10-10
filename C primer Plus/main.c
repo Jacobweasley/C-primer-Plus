4f49f874e43c12533188504e87971bf28464b481
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-10 14:14:32 +0800

    在 C语言中,使用越界的下标访问数组的话 gcc 编译器并不会管下标是否 安全,不像 Swift 那样有溢出保护.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,14 +1,24 @@
-
 #include <stdio.h>
-#define MONTHS 12
-int main(void)
-{
-    int days[MONTHS] = {31, 28, [4] = 31, 30, 31, [1] = 29};
+#define SIZE 4
+int main(void){
+    int value1 = 44;
+    int arr[SIZE];
+    int value2 = 88;
     int i;
     
-    for (i = 0; i < MONTHS; i++)
-    printf("%2d %d\n", i + 1, days[i]);
+    printf("value1 = %d, value2 = %d\n", value1, value2);
+    for (i = -1; i <= SIZE; i++) {
+        arr[i] = 2 * i + 1;
+    }
+    for (i = -1; i < 7; i++) {
+        printf("%2d %d\n", i, arr[i]);
+    }
+    
+    printf("value1 = %d, value2 = %d\n", value1, value2);
+    printf("address of arr[-1] : %p\n", &arr[-1]);
+    printf("address of arr[4] : %p\n", &arr[4]);
+    printf("address of value1 : %p\n", &value1);
+    printf("address of value2 : %p\n", &value2);
     
     return 0;
-    
 }
