0c427a67870aa0cd726511a750ba999934d28d60
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-10 15:06:00 +0800

    在 C语言中对一个指针 + 1 的作用是指将指针往内存后移动一个 类型所占的内存位置,也就是说 指针+1的意思就是跳指到下一个类型内存地址(这里指的两个类型是相同的类型.)

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,39 +1,19 @@
 #include <stdio.h>
-#define MONTHS 12
-#define YEARS 5
+#define SIZE 4
 int main(void)
 {
-    const float rain[YEARS][MONTHS] =
-    {
-        {2.3, 2.4, 2.4, 3.2, 2.2, 4.2, 2.3, 5.3, 2.1, 5.3, 2.3, 4.5},
-        {2.3, 2.4, 2.4, 3.2, 2.2, 4.2, 2.3, 5.3, 2.1, 5.3, 2.3, 4.5},
-        {2.3, 2.4, 2.4, 3.2, 2.2, 4.2, 2.3, 5.3, 2.1, 5.3, 2.3, 4.5},
-        {2.3, 2.4, 2.4, 3.2, 2.2, 4.2, 2.3, 5.3, 2.1, 5.3, 2.3, 4.5},
-        {2.3, 2.4, 2.4, 3.2, 2.2, 4.2, 2.3, 5.3, 2.1, 5.3, 2.3, 4.5}
-    };
-    int year, month;
-    float subtot, total;
-    
-    printf(" YEAR    RAINFALL   (inches)\n");
-    for (year = 0, total = 0; year < YEARS; year++) {
-        for (month = 0, subtot = 0; month < MONTHS; month++)
-        subtot += rain[year][month];
-        printf("%5d %15f\n", 2010 + year, subtot);
-        total += subtot;
-    }
-    printf("\nThe yearly average is %.1f inches.\n\n", total / YEARS);
-    printf("MONTHLY AVERAGES: \n\n");
-    printf(" Jan Feb Mar Apr May Jun Jul Aug Sep Oct");
-    printf(" Nov Dec\n");
-    
-    for (month = 0; month < MONTHS; month++)
-    {
-        for (year = 0, subtot = 0; year < YEARS; year++) {
-            subtot += rain[year][month];
-        }
-        printf("%4.lf ", subtot / YEARS);
-    }
-    printf("\n");
+    short dates[SIZE];
+    short * pit;
+    short index;
+    double bills[SIZE];
+    double * ptf;
+    pit = dates;
+    ptf = bills;
+    printf("%23s %15s\n", "short", "double");
+    //这个打印语句实际上只是单纯的打印字符串的语句而已
+    for (index = 0; index < SIZE; index++)
+    printf("pointers + %d: %10p %10p\n", index, pit + index, ptf + index);
     
     return 0;
 }
+
