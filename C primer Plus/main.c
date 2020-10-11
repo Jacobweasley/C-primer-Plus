5d25ce13c5ecc5da88b0316200486092bd72496f
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-11 12:38:37 +0800

    多维数组的解包需要多重的取值操作符 * 号. 在遇到多维数组的时候,就只要记住一句还,多维数组其实只是数组的项里装着数组而已,只是一种嵌套的运算.注意多维数组的维数,维数对应着解包到底取出实际的值的多所需要的解包数.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
-#define SIZE 5
-void show_array(const double ar[], int n);
-void mult_array(double ar[], int n, double mult);
 int main(void)
 {
-    double dip[SIZE] = { 20.0, 17.66,8.2, 15.3, 22.22 };
-    printf("The original dip array:\n");
-    show_array(dip, SIZE);
-    mult_array(dip, SIZE, 2.5);
-    printf("The dip array agter calling mult_array():\n");
-    show_array(dip, SIZE);
+    int zippo[4][2] = {{2, 4}, {6, 8}, {1, 3}, {5, 7}};
+    
+    printf("   zippo = %p,    zippo + 1 = %p\n", zippo, zippo + 1);
+    printf("zippo[0] = %p, zippo[0] + 1 = %p\n", zippo[0], zippo[0] + 1);
+    printf("zippo[0][0] = %d\n", zippo[0][0]);
+    printf("  *zippo[0] = %d\n", *zippo[0]);
+    printf("    **zippo = %d\n", **zippo);
+    printf("      zippo[2][1] = %d\n", zippo[2][1]);
+    printf("*(*(zippo+2) + 1) = %d\n", *(*(zippo + 2) + 1));
     
     return 0;
-}
-
-void show_array(const double ar[], int n)
-{
-    int i;
-    
-    for (i = 0; i < n; i++)
-    printf("%8.3f", ar[i]);
-    putchar('\n');
-    
-}
-
-void mult_array(double ar[], int n, double mult)
-{
-    int i;
-    
-    for (i = 0; i < n; i++)
-    ar[i] *= mult;
-    
 }
