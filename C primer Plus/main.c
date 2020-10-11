a343b31fdfa50b73b645048af5ac192b45979e28
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-11 16:12:37 +0800

    其实在入参的时候并不能只入参一个多维数组这样的,在入参的时候需要有明确的表示该数组的内存地址的始与终.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,69 +1,53 @@
 #include <stdio.h>
 #define ROWS 3
 #define COLS 4
-void sum_row3(int ar[][COLS], int rows);
-void sum_cols(int [][COLS], int);
-int sum2d(int (*ar)[COLS], int rows);
+int sum2d(int rows, int cols, int ar[rows][cols]);
 int main(void)
 {
+    int i, j;
+    int rs = 3;
+    int cs = 10;
     int junk[ROWS][COLS] = {
         { 2, 4, 6, 8 },
         { 3, 5, 7, 9 },
-        { 12, 10, 8, 6 }
+        { 12, 10, 8, 6}
     };
     
-    sum_row3(junk, ROWS);
-    sum_cols(junk, ROWS);
-    printf("Sum of all elements = %d\n", sum2d(junk, ROWS));
+    int morejunk[ROWS - 1][COLS + 2] = {
+        { 20, 30, 40, 50, 60, 70},
+        { 5, 6, 7, 8, 9, 10}
+    };
+    
+    int varr[rs][cs];
+    
+    for (i = 0; i < rs; i++)
+    for (j = 0; j < cs; j++)
+    varr[i][j] = i * j + j;
+    
+    printf("3x4 array\n");
+    printf("Sum of all elements %d\n", sum2d(ROWS, COLS, junk));
+    
+    printf("2x6 array\n");
+    printf("Sum of all elements = %d\n", sum2d(ROWS - 1, COLS + 2, morejunk));
+    
+    printf("3x10 VLA\n");
+    printf("Sum of all elements = %d\n", sum2d(rs, cs, varr));
     
     return 0;
-}
-
-void sum_row3(int ar[][COLS], int rows)
-{
-    int r;
-    int c;
-    int tot;
     
-    for (r = 0; r < rows; r++ )
-    {
-        tot = 0;
-        for (c = 0; c < COLS; c++ )
-        {
-            tot += ar[r][c];
-        }
-        printf("row %d: sum = %d\n", r, tot);
-    }
 }
 
-
-void sum_cols(int ar[][COLS], int rows)
-{
-    int r;
-    int c;
-    int tot;
-    
-    for (c = 0; c < COLS; c++ )
-    {
-        tot = 0;
-        for (r = 0; r < rows; r++) {
-            tot += ar[r][c];
-        }
-        printf("col %d: sum = %d\n", c, tot);
-    }
-}
-
-int sum2d(int ar[][COLS], int rows)
+int sum2d(int rows, int cols, int ar[rows][cols])
 {
     int r;
     int c;
     int tot = 0;
     
     for (r = 0; r < rows; r++)
-    for (c = 0; c < COLS; c++ )
+    for (c = 0; c < cols; c++)
     tot += ar[r][c];
     
     return tot;
-    
-    
 }
+
+
