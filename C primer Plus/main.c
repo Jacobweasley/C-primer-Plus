69ce4286b07459f010f524d4fa3428adec6d4964
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-11 15:53:30 +0800

    将多维数组入参的到一个函数使用的时候,在函数的定义模型的阶段,入参的多维数组时是需要将多维数组的最后一样填上就好,需要入参的底层时多少个项的就写多少. 记住函数的定义模型阶段都只是为来在内存中划出一定的内存空间.并且在真正的定义阶段的时候其实已经是有实参进入了,所以入参多维数组,只需要定义最后放括号就就好,其他的在实参进入的时候编译器通过运算都可以得出. 其实我怀疑其实多维数组并不只是不能省略最后一个放括号,其实因该是除了第一个放括号可以省略意外第一个方括号后面的项都不可以省略. 其实入参一个多维数组,除了第一个方括号可以省略以外其实在模型阶段,多维数组的形参名字也是可以省略的,与入参普通的变量或常量都是一样的,又因为数组名就是数组的第一个项的内存地址,所以使用一个指向数组的第一个项的指针,也就是一个数组类的指针也是可以的.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,21 +1,69 @@
 #include <stdio.h>
-
+#define ROWS 3
+#define COLS 4
+void sum_row3(int ar[][COLS], int rows);
+void sum_cols(int [][COLS], int);
+int sum2d(int (*ar)[COLS], int rows);
 int main(void)
 {
-    int zippo[4][2] = {{2, 4}, {6, 8}, {1, 3}, {5, 7}};
-    int (*pz)[2];//这是一个数组类型的指针,指向一个包含两个项的数组.
-    //为什么样有括号,因为方括号的计算优先级高于 * 号
-    //在声明语句中,声明一个了个项的数组类型指针.在 + 1 以后是跳过 一个 二项数组 的内存距离
-    pz = zippo;
-    //这种赋值指针的赋值语句,实际上就是为指针提供了一个内存地址,这个内存地址是用来提供给该指针用过开头使用的,也就是说,赋值语句给予指针的其实只是一个具体的内存地址,而指针自身的类型,结合该具体的内存地址则划出了一个内存区间,这才是该指针真正指向的.
+    int junk[ROWS][COLS] = {
+        { 2, 4, 6, 8 },
+        { 3, 5, 7, 9 },
+        { 12, 10, 8, 6 }
+    };
     
-    printf("    pz = %p,    pz + 1 = %p\n",   pz, pz + 1);
-    printf(" pz[0] = %p, pz[0] + 1 = %p\n", pz[0], pz[0] + 1);
-    printf("  *pz[0][0] = %d\n", pz[0][0]);
-    printf("     *pz[0] = %d\n",  *pz[0]);
-    printf("       **pz = %d\n", **pz);
-    printf("            pz[2][1] = %d\n", pz[2][1]);
-    printf("    *(*(pz + 2) + 1) = %d\n", *(*(pz + 2) + 1));
+    sum_row3(junk, ROWS);
+    sum_cols(junk, ROWS);
+    printf("Sum of all elements = %d\n", sum2d(junk, ROWS));
     
     return 0;
+}
+
+void sum_row3(int ar[][COLS], int rows)
+{
+    int r;
+    int c;
+    int tot;
+    
+    for (r = 0; r < rows; r++ )
+    {
+        tot = 0;
+        for (c = 0; c < COLS; c++ )
+        {
+            tot += ar[r][c];
+        }
+        printf("row %d: sum = %d\n", r, tot);
+    }
+}
+
+
+void sum_cols(int ar[][COLS], int rows)
+{
+    int r;
+    int c;
+    int tot;
+    
+    for (c = 0; c < COLS; c++ )
+    {
+        tot = 0;
+        for (r = 0; r < rows; r++) {
+            tot += ar[r][c];
+        }
+        printf("col %d: sum = %d\n", c, tot);
+    }
+}
+
+int sum2d(int ar[][COLS], int rows)
+{
+    int r;
+    int c;
+    int tot = 0;
+    
+    for (r = 0; r < rows; r++)
+    for (c = 0; c < COLS; c++ )
+    tot += ar[r][c];
+    
+    return tot;
+    
+    
 }
