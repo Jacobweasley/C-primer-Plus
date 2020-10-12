e53ed4f6d75123eecc6babc784de9062f4c03acb
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-12 15:23:59 +0800

    在 C语言中使用 get() 函数来过去输入的字符的话,就会有一个很严重的问题,因为 get() 函数并不会检查 get( 入参) 入参中的字符串数组的实际可容纳内字符数,其背后的原理是因为 get(入参)  函数只会引用入参数组的开始的内存地址,也就是入参数组的名称常量,也就是该数组的实际内存地址的开头地址.而不会管齐什么时候结束.所以在我们输入的字符串过长的时候就会造成过度的内存错误擦写.从而引发程序错误 在get()被运行时,她只会获取入参所要存储的实际内存地址的开始地址,而我们实际的输入先会被缓冲区所获取然后在我们是入 回车的时候才会被填入对应的内存地址,但是这就是问题的所在,填入的时候会保证所有我们所输入的字符都会被完整的拷贝到,对应的地址,但是 get(入参) 并不会检查他的入参数组的实际能不能容纳这些字符串,如果能容纳就完事大吉,如果不能容纳,那就不好说了,能够容纳的最大的值被超过了以后多出的部分也会被强行的写入,所以在实际情况下的话,如果强行写入的地方正好有别的程序的内存 数据的话,就会引发奔溃,威胁内存安全,如果强行写入的内存地址正好还没哟被使用,那么就一会其他的程序所覆盖,导致内存数据不准确

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#define SRTLEN 3
 int main(void)
 {
-    const char * mesg = "Don't be a fool!";
-    const char * copy;
+    char words[SRTLEN];
     
-    copy = mesg;
-    printf("%s\n", copy);
-    printf("mesg = %s; &mesg = %p; value = %p\n", mesg, &mesg, mesg);
-    printf("copy = %s; &copy = %p; value = %p\n", copy, &copy, copy);
+    puts("Enter a string, please.");
+    gets(words);
+    printf("Your string twice;\n");
+    printf("%s\n", words);
+    puts(words);
+    puts("Done.");
+    
+    return 0;
 }
+
