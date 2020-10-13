316d99570c0ad3bfaccc08e5897f03e851e02e60
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-13 16:09:39 +0800

    strcmp() 函数在 两个入参字符串相同的时候返回 0 ,不相同的时候返回非0 的数. 也许你也注意到了为什么别的判断类型函数都是在为真的时候返回 真,所以这一定是有原因的: strcmp会逐个比较两个字符串中的每个字符,直到发现两个不相同的字符为止,如果第一个字符串的字符在第二个字符串的字符的前面,strcmp()就会返回负数,反之亦然则会返回正数. 并且 strcmp() 函数比较的是字符串而不是比较的是字符,所以,两个入参都必须是字符串,也就是两个字符串都是必须要带 \0 空字符的.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 30
-#define BUGSIZE 13
-char * s_gets(char * st, int n );
+#define ANSWER "Grant"
+#define SIZE 40
+char * s_gets(char * st, int n);
 
 int main(void)
 {
-    char flower[SIZE];
-    char addon [] = "s smell like ild shoes.";
-    char bug[BUGSIZE];
-    int available;
-    puts("What is your favorite flower?");
-    s_gets(flower, SIZE);
-    if ((strlen(addon) + strlen(flower) + 1) <= SIZE) {
-        strcat(flower, addon);
+    char try[SIZE];
+    
+    puts("Who is buried in Grant's tomb?");
+    s_gets(try, SIZE);
+    while (strcmp(try, ANSWER) != 0) {
+        puts("No, that's wrong. Try again.");
+        s_gets(try, SIZE);
     }
-    puts(flower);
-    puts("What is your favorite bug?");
-    s_gets(bug, BUGSIZE);
-    available = BUGSIZE - strlen(bug) - 1;
-    strncat(bug, addon , available);
-    puts(bug);
+    puts("That's right!");
     
     return 0;
 }
+
 char * s_gets(char * st, int n)
 {
     char * ret_val;
     int i = 0;
     
     ret_val = fgets(st, n, stdin);
-    if (ret_val) {
-        while (st[i] != '\n' && st[i] != '\0')
+    if (ret_val)
+    {
+        while (st[i] != '\n' && st[i] != '\0') {
             i++;
+        }
         if (st[i] == '\n')
             st[i] = '\0';
         else
@@ -41,6 +38,3 @@ char * s_gets(char * st, int n)
     }
     return ret_val;
 }
-
-
-
