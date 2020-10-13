8a97553154a60aec33e611f31856411607f6807c
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-13 16:33:30 +0800

    如果使用多维数组来存储多条字符串的话,原理也是一样的,只有数组的最低层也就是最后一个数组下标代表的才是实际数据,在这里用多维数组来存储字符串的例子里,最有一组下标就代表,存储这些字符串的字符数组有多大的空间. 前面的下标的概念像是排列的意思吧.但是所到底,在内存中,这些数据都是连续存储的. 当多维数被一维解读的话,就相当于解读该多维数组的第二维的数组,而如果这一层里面存储的是就是字符串就相当于定位那一条字符串的意思.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include <string.h>
-#define ANSWER "Grant"
-#define SIZE 40
+#include <stdio.h>
+#define SIZE 80
+#define LIM 10
+#define STOP "quit"
 char * s_gets(char * st, int n);
 
 int main(void)
 {
-    char try[SIZE];
+    char input[LIM][SIZE];
+    int ct = 0;
     
-    puts("Who is buried in Grant's tomb?");
-    s_gets(try, SIZE);
-    while (strcmp(try, ANSWER) != 0) {
-        puts("No, that's wrong. Try again.");
-        s_gets(try, SIZE);
+    printf("Enter up to %d lines (type quit to quit):\n", LIM);
+    while (ct < LIM && s_gets(input[ct], SIZE) != NULL && strcmp(input[ct], STOP) != 0) {
+        ct++;
     }
-    puts("That's right!");
+    printf("%d strings entered\n", ct);
     
     return 0;
 }
@@ -25,16 +25,17 @@ char * s_gets(char * st, int n)
     int i = 0;
     
     ret_val = fgets(st, n, stdin);
-    if (ret_val)
-    {
+    if (ret_val){
         while (st[i] != '\n' && st[i] != '\0') {
             i++;
         }
-        if (st[i] == '\n')
+        if (st[i] == '\n') {
             st[i] = '\0';
+        }
         else
             while (getchar() != '\n')
                 continue;
     }
+    
     return ret_val;
 }
