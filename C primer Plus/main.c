3c791177bf47275b3c45bcec49536f1fa19a0ae6
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-13 13:11:35 +0800

    scanf() 函数的主要作用原理是检测我们所输入的字符串中的 空格键,也就是说空格字符.在 我们的输入标准字符前没有其他数字修饰的情况下,scanf () 函数是只会主动检测我们输入到缓冲区中的空格字符. 会主动的使用空格字符为间隔分割字符串,并完成赋值到对应的字符数组中. 当字符串标准字符前有数组修饰的时候, 如 %5s 则表示,检测输入到缓冲区中的5个字符,如果在检测的这五个字符里面有 空格字符的话,则会如上一样直接创建字符串传入对应的字符数组中,而剩下的字符还会存在于缓冲区中,如果这时有另外一个,字符串标准字符的话,剩下的将会被它所引用输入到字符数组中.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,31 +1,13 @@
 
 #include <stdio.h>
-#define STLEN 10
 int main(void)
 {
-    char words[STLEN];
+    char name1[11], name2[11];
+    int count;
     
-    int i;
+    printf("Please enter 2 names.\n");
+    count = scanf("%5s %10s", name1, name2);
+    printf("I read the %d names %s and %s.\n", count, name1, name2);
     
-    puts("Enter strings (empty line to quit):");
-    while (fgets(words, STLEN, stdin) != NULL && words[0] != '\n') {
-        i = 0;
-        while (words[i] != '\n' && words[i] != '\0') {
-            i++;
-        }
-        if (words[i] == '\n') {
-            words[i] = '\0';
-        }
-//        } else {
-//            while (getchar() != '\n')
-//                continue;
-//        }
-//        puts(words);
-        fputs(words, stdout);
-        
-        
-    }
-    puts("done");
     return 0;
-    
 }
