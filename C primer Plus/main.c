28ad781ed289bd83a88ad78d120db15a70539b93
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-13 17:35:00 +0800

    strcpy() 函数顾名思义就是用来拷贝函数的,strcpy() 函数用于接收两个入参,其中的第一个入参叫目标入参,而第二个入参叫过拷贝入参,而strcpy () 函数的作用就是用来将第二个入参也就是将第二个字符串拷贝到第第一个入参当中, 所以一般的第一个入参一般是一个多维数组的字符串数组,并且拥有足够的空间来容纳可能的第二入参,即第二个入参所指向的字符串, 拷贝字符串不像是拷贝整形那样直接用 = 赋值号. 因为在 C语言中,字符串并不算事与原生类型,并没有得到底层的支持, 同样的 strcpy () 也对应的拥有 strncpy() 函数,它对比 strcpy()多了第三个函数,这第三个函数是用来限制拷贝第二参数的字符为数的,但是与所有的字符串处理函数一样,strncpy() 也需要考虑 字符串后面的 \0 因为在实际情况中如果如果使用 strncpy() 函数限制拷贝第二入参的字符串位数的的时候,有可能,第二字符串中的长度超过了所限制的位数于是拷贝到目标数组中的字符串也许就会没有 \0 .所以为了避免这样的情况发生,最好在设置拷贝位数限制的时候将限制的位数设置为目标数组的接受上限 - 1 位,然后在执行判断是否拷贝有 \0 如果没有拷贝到的话,就在 目标数组中补上.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-#define LISTSIZE 6
+#define SIZE 40
+#define LIM 5
+char * s_gets(char * st, int n);
+
 int main(void)
 {
-    const char * list[LISTSIZE] =
+    char qwords[LIM][SIZE];
+    char temp[SIZE];
+    int i = 0;
+    
+    printf("Enter %d words beginning with q:\n", LIM);
+    while (i < LIM && s_gets(temp, SIZE))
     {
-        "astronomy", "astounding",
-        "astrophysics", "ostracize",
-        "asterism", "astrophobia"
-    };
-    
-    int count = 0;
-    int i;
-    
-    for (i = 0 ; i < LISTSIZE; i++) {
-        if (strncmp(list[i], "astro", 5) == 0) {
-            printf("Found: %s\n", list[i]);
-            count++;
+        if (temp[0] != 'q') {
+            printf("%s doesn't begin with q!\n", temp);
+        }
+        else
+        {
+            strcpy(qwords[i], temp);
+            i++;
         }
     }
-    printf("The list contained %d words beginning"" with astro.\n", count);
+    puts("Here are the words accepted:");
+    for (i = 0; i < LIM; i++) {
+        puts(qwords[i]);
+    }
     return 0;
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    int i = 0;
+    
+    ret_val = fgets(st , n, stdin);
+    if (ret_val){
+        while (st[i] != '\n' && st[i] != '\0') {
+            i++;
+        }
+        if (st[i] == '\n') {
+            st[i] = '\0';
+        }
+        else
+            while (getchar() != '\n') {
+                continue;
+            }
+    }
+    return ret_val;
 }
