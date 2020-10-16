1bb08b3a35b352c86d794023fa5571c3a12cce1b
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-16 17:02:25 +0800

    这里主要引出了 关于内存管理的三个概念, 一个是静态存储区,另为一个是动态存储区  还有一个是 自动变量使用, 其中的不同点就是 前两个是两个极端,只有最后一个是有由程序自己管理的 叫做栈 先说第一个 静态存储区  该区域主要用于存放 文件变量灯随程序的启动而加载,随程序的退出而 卸载的内存  值得注意的是 一个程序中的 所有 字符串的字面量,也就是说实际的字符串的值都是存储在 静态内存当中的. 而后面两个分别就是 堆 和 栈了 动态存储区又叫作 堆 ,在该内存中的数据都是由程序员自己分配管理的, 在该区域的内存通常因为 有着程序无法干预的内存申请以及退出方式 其主动权都属于 程序员,所以相对的  位于堆中的数据又或者是可用的内存块都是支离破碎的,所以相应的运行效率也是最低的. 而自动内存区域 也叫做 栈 栈中的内存信息是程序自己管理的 ,里面都是一些 函数 又或者是区域变量 ,随着函数的启用而加载,随着函数的退出而卸载, 所以在该区域的信息 的运行效率是相对较高的,但当然的 位于 静态内存区的才是效率最高的.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,44 +1,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int static_store = 30;
+const char * pcg = "String Literal";
 
-int main(void)
+int main()
 {
-    double * ptd;
-    int max;
-    int number;
-    int i = 0;
+    int auto_store = 40;
+    char  auto_string [] = "Auto char Array";
+    int * pi;
+    char * pcl;
     
-    puts("What is the maximum number of type double entries?");
-    if (scanf("%d", &max) != 1) {
-        puts("Number not correctly entered -- bye.");
-        exit(EXIT_FAILURE);
-    }
-    ptd = (double *) malloc(max * sizeof (double));
-    if (ptd == NULL) {
-        puts("Memory allocation failed. Goodbye.");
-        exit(EXIT_FAILURE);
-    }
-    puts("Enter the values (q to quit): ");
-    while (i < max && scanf("%lf", &ptd[i]) == 1)
-        ++i;
-    printf("Here are your %d entries:\n", number = i);
-    for (i = 0;  i < number; i++) {
-        printf("%7.2f", ptd[i]);
-        if (i % 7 == 6)
-            putchar('\n');
-    }
-    if (i % 7 != 0)
-        putchar('\n');
-    puts("Done.");
-    free(ptd);
+    pi = (int * ) malloc(sizeof(int));
+    *pi =  35;
+    pcl = (char * ) malloc(strlen("Dynamic String") + 1);
+    strcpy(pcl, "Dynamic String");
+    
+    printf("static_store: %d at %p\n", static_store, &static_store);
+    printf(" auto_store: %d at %p\n", auto_store, &auto_store);
+    printf("      *pi: %d at %p\n", *pi, pi);
+    printf("  %s at %p\n", pcg, pcg);
+    printf("  %s at %p\n", auto_string, auto_string);
+    printf("  %s at %p\n", pcl, pcl);
+    printf("  %s at %p\n", "Quoted String", "Quoted String");
+    free(pi);
+    free(pcl);
     
     return 0;
     
 }
-//除了可以使用malloc () 函数来分配内存, 还可以使用 calloc () 函数来申请内存
-//calloc () 函数于malloc() 函数的区别就是 calloc () 函数比 malloc () 函数多了一个入参, 该入参为 ungicter int 类型的 起作用只是啦确定需要多少个 二参 所指定的内存区间.另为一点就是 通过 calloc () 函数所申请的内存是会被默认的将所申请的内存都初始化为 0 的.
-//calloc () 函数所申请的内存空间的释放也是使用 free () 函数
-//使用 malloc () 函数 又或是 calloc () 函数所申请的内存空间都是用free() 函数来释放,但是 free 所入参的指针可以于 依赖定义 malloc 或 calloc 是所非配的不同,但是所指向的内存地址是要相同的.
 
