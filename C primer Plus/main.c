4b46238c2c2dd113e525693b2ab831876d5920f4
Jacobweasley <36145467+Jacobweasley@users.noreply.github.com>
2020-10-16 16:12:15 +0800

    在 C 语言中 一种内存非配的函数 即 : malloc () 函数 ,该函数是包含在头文件, stdlib.h 的头文件里面的. 要想使用 malloc () 函数来分配内存就需要配合使用 free () 函数来销毁内存,释放内存. malloc () 函数 接收一个入参, 该入参为 int 类型,用于确定需要分配的内存的大小. 但是实际上,每台电脑的int 类型的实际大小都会有不同,所以配合 sizeof ()  函数来确定 所需内存的大小,在使用 乘法表达式,来一次获取所个 类型大小. 使用 malloc () 函数所获取到的内存是没有类型的实际内存地址 , 即获取的是未指向类型 void  类型内存地址,所以为了接受 malloc () 函数所产生的 内存地址.即使用 指针来接收 malloc () 函数所产生的内存地址信息, 但又因为 malloc () 函数所产生的内存是没有类型的,所以一般的,会在 malloc () 前使用 ( 强制类型转换 )  来将 malloc () 函数所产生的 void 类型的内存 转换成 所需要的类型, 当然也可以使用 void 类型的指针来直接接收该内存, 但是这也只是将强制类型转换的任务讲到后面的代码中而已.  这里补充一下,指针的类型转换的 方法  因为指针本身属于一种 类型 而她的标识符为 * 但她又有特定的存储类型 入 double 类型,所以在强制类型转换的时候  的语法为 ( double * )  表示 强制转换为 double 类型的指针 相应的 free () 函数的职责就是为了释放 malloc () 函数所产生的 内存 而存在的. free() 函数也是只接受一个入参,即接收 一个内存地址(而承载 内存地址的自然就是 指针啦.)同过入参的 内存地址, free() 就可以从内存中被释放.所以 malloc () 函数所产生的 内存 的生存周期为 malloc 到 free ,也即是自由周期. 注意 : 在 c 语言中可以不使用 刚才提到的类型转换内存地址类型转换.但是 在 C++ 中是必须的,所以 使用强制类型转换可以更好的创建 C++ 类型程序. 还有另为一个知识点那就是 用于 退出当前程序的 exit 函数 , exit()  函数 就收两个特别的常量 一个是 EXIT_FAILURE 另为一个是 EXIT_SUCCESS 这两个常量配合 exit () 函数可以终止程序的运行, 并在屏幕上打印出相应的错误信息. exit () 是用于在程序 走向错误时 ,使用 exit () 来终止程序 并打印出错误,  在程序走向发生意外的错误的时候相当有用, 比如在 使用 malloc() 函数来分配内存空间的时候是有可能会失败的, 在 malloc () 函数分配内存失败的时候是会 返回一个 常量 NULL 就表示 malloc () 内存分配失败. 如果程序继续执行下去的话就会 引发报错,所以在这种可以遇见的错误面前,使用exit () 函数提前推出函数 避免报错 则是一种更为聪明的方法.除非有 方法可以纠正这种错误.

--- C primer Plus/main.c
+++ C primer Plus/main.c
@@ -1,24 +1,39 @@
+
 #include <stdio.h>
-int units = 0;
-void critic(void);
-int main(void){
-    extern int units;
+#include <stdlib.h>
+
+
+int main(void)
+{
+    double * ptd;
+    int max;
+    int number;
+    int i = 0;
     
-    printf("How many pounds to a firkin of butter?\n");
-    scanf("%d", &units);
-    while (units != 56) {
-        critic();
+    puts("What is the maximum number of type double entries?");
+    if (scanf("%d", &max) != 1) {
+        puts("Number not correctly entered -- bye.");
+        exit(EXIT_FAILURE);
     }
-    printf("You must have looked it up!\n");
+    ptd = (double *) malloc(max * sizeof (double));
+    if (ptd == NULL) {
+        puts("Memory allocation failed. Goodbye.");
+        exit(EXIT_FAILURE);
+    }
+    puts("Enter the values (q to quit): ");
+    while (i < max && scanf("%lf", &ptd[i]) == 1)
+        ++i;
+    printf("Here are your %d entries:\n", number = i);
+    for (i = 0;  i < number; i++) {
+        printf("%7.2f", ptd[i]);
+        if (i % 7 == 6)
+            putchar('\n');
+    }
+    if (i % 7 != 0)
+        putchar('\n');
+    puts("Done.");
+    free(ptd);
     
     return 0;
     
 }
-
-void critic(void)
-{
-    printf("No lock, my friend.Try again.\n");
-    scanf("%d", &units);
-    
-}
-//
